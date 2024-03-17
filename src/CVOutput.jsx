bfd8628119019355ae7a04f7ccdaf6324af09fad
Ahmad Faris <149571714+ahmadfariszk@users.noreply.github.com>
2024-03-17 08:56:44 +0800

    Added date formatter

--- src/CVOutput.jsx
+++ src/CVOutput.jsx
@@ -4,6 +4,15 @@ import "./CVOutput.css";
 function CVOutput(props) {
   const { generalData, workData, educationData } = props;
 
+  const limitedWorkData = [...workData.slice(0, 3)]; //limit data entry to 3
+  
+  const formatDate = (dateString) => {
+    const parsedDate = new Date(dateString);
+    const month = parsedDate.getMonth() + 1; // Adding 1 because getMonth() returns zero-based month index
+    const year = parsedDate.getFullYear();
+    return `${month.toString().padStart(2, "0")}/${year}`;
+  };
+
   return (
     <div className="CVContainer">
       {generalData.map((info) => (
@@ -17,35 +26,36 @@ function CVOutput(props) {
           </div>
         </div>
       ))}
-      {generalData.map((info)=>(<div key={info.name} className="contactsContainer">
-        <div>{info.email}</div>
-        <div>{info.phoneNumber}</div>
-        <div>{info.linkedIn}</div>
-      </div>))}
+      {generalData.map((info) => (
+        <div key={info.name} className="contactsContainer">
+          <div>{info.email}</div>
+          <div>{info.phoneNumber}</div>
+          <div>{info.linkedIn}</div>
+        </div>
+      ))}
       <div className="workContainer">
         <div className="workContainerHeader">Work Experience</div>
-        <div className="workSubContainer">
-          <div className="workTitle">Senior Work Position 1</div>
-          <div className="workCompany">Company Name Sdn Bhd 1</div>
-          <div className="workDate">
-            <span>DDMMYYYY</span> to <span>DDMMYYYY</span>
+        {limitedWorkData.map((work) => (
+          <div key={work.jobTitle} className="workSubContainer">
+            <div className="workTitle">{work.jobTitle}</div>
+            <div className="workCompany">{work.company}</div>
+            <div className="workDate">
+              <span>{formatDate(work.dateStart)}</span> - <span>{formatDate(work.dateEnd)}</span>
+            </div>
+            <ul className="workDescription">
+              <li>
+                {work.jobduty1}{" "}
+              </li>
+              <li>
+                {work.jobduty2}{" "}
+              </li>
+              <li>
+                {work.jobduty3}{" "}
+              </li>
+            </ul>
           </div>
-          <ul className="workDescription">
-            <li>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor{" "}
-            </li>
-            <li>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor{" "}
-            </li>
-            <li>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor{" "}
-            </li>
-          </ul>
-        </div>
-        <div className="workSubContainer">
+        ))}
+        {/* <div className="workSubContainer">
           <div className="workTitle">Senior Work Position 2</div>
           <div className="workCompany">Company Name Sdn Bhd 2</div>
           <div className="workDate">
@@ -86,7 +96,7 @@ function CVOutput(props) {
               eiusmod tempor{" "}
             </li>
           </ul>
-        </div>
+        </div> */}
       </div>
       <div className="educationContainer">
         <div className="educationContainerHeader">Education</div>
